3958f8b3e379364bb3b6160f967b98b2aa307930
Ooke Nivalainen <ooke.nivalainen@gmail.com>
2020-04-05 16:17:09 +0300

    Lisätty tehtävä2

--- tulostus.js
+++ tulostus.js
@@ -1,2 +1,26 @@
+module.exports.tulostaTahtia = tulostaTahtia
+module.exports.tulostaTulos = tulostaTulos
 
-const tulostaTahtia = (maara) => console.log('*' * maara)
+const tulostaTahtia = (maara) => {
+    var result= '';
+for (var i = 0; i < maara; i++) {
+    result = result + ('* ')
+    }
+    console.log(result)
+};
+
+//tulostaTahtia(10);
+
+const tulostaTulos = (tulos) => {
+   var tuloste = String(tulos)
+   var x = tuloste.length;
+   var result= '';
+    for( var i = 0; i < x; i ++) {
+    result = result + ('-')
+    }
+    console.log(result)
+    console.log(tuloste)
+    console.log(result)
+}
+
+//tulostaTulos(34523452)
